e7d0392a839c0fa8611557ec7ec31d77b8ac7584
Pierre Fils <epierrefils@gmail.com>
2023-01-27 22:36:32 -0500

    Add Test in pom

--- src/main/java/com/careerdevs/productservice/controller/ProductController.java
+++ src/main/java/com/careerdevs/productservice/controller/ProductController.java
@@ -1,4 +1,34 @@
 package com.careerdevs.productservice.controller;
 
+
+import com.careerdevs.productservice.dto.ProductResponse;
+import com.careerdevs.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductResponse productRequest){
+        productService.createProduct(productRequest);
+
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+       return productService.getAllProducts();
+    }
+
+
+
 }
--- src/test/java/com/careerdevs/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/careerdevs/productservice/ProductServiceApplicationTests.java
@@ -2,9 +2,21 @@ package com.careerdevs.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.4");
+
+	static  void setProperties (DynamicPropertyRegistry dymDnamicPropertyRegistry){
+		dymDnamicPropertyRegistry.add("spring,data.mongodb.uri",mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
 	void contextLoads() {
